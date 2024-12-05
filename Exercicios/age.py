5aac1be807093f5ae50ec16134bd0224093debbe
VulpeOlesea <leseavulpe@gmail.com>
2024-12-05 10:52:48 +0000

    add exercicios 4/12/24

---
+++ Exercicios/age.py
@@ -0,0 +1,12 @@
+'''
+Exercicio 1:
+· Escreva um programa designado de age.py que questiona a um
+utilizador o ano de nascimento, e retorna a idade deste no presente
+ano - 2024
+'''
+
+ano_nascimento = int(input("Insira o seu ano de nascimento: "))
+ano_atual = 2024
+idade = ano_atual - ano_nascimento
+
+print(f"Sua idade: {idade}")
---
+++ Exercicios/nome.py
@@ -0,0 +1,12 @@
+'''
+· Exercicio 2:
+· Escreva um progrma que questiona um user sobre o seu nome e
+retorna o seu nome. Será devolvido a quantidade de letras do nome
+· Nota: recorde-se da function len( ... ) para determinar o tamanho de
+uma String
+'''
+
+nome = input("Insira o seu nome: ")
+letras_quantidade = len(nome)
+
+print(f"O seu nome contém {letras_quantidade} letras.")
